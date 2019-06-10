5e7ecdc5434697c4e614d55e0ca128a2874de2d9
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-06-09 22:34:05 -0300

    Create exer7.php

---
+++ exer7.php
@@ -0,0 +1,59 @@
+<?php
+function MesNum ($mes) {
+ switch ($mes){
+     case 1;
+     echo "Janeiro"."\n";
+     break;
+
+     case 2;
+     echo "Fevereiro"."\n";
+     break;
+
+     case 3;
+     echo "MarÃ§o"."\n";
+     break;
+
+     case 4;
+     echo "Abril"."\n";
+     break;
+
+     case 5;
+     echo "Maio"."\n";
+     break;
+
+     case 6;
+     echo "Junho"."\n";
+     break;
+
+     case 7;
+     echo "Julho"."\n";
+     break;
+
+     case 8;
+     echo "Agosto"."\n";
+     break;
+     
+     case 9;
+     echo "Setembro"."\n";
+     break;
+
+     case 10;
+     echo "Outrubo"."\n";
+     break;
+
+     case 11;
+     echo "Novembro"."\n";
+     break;
+
+     case 12;
+     echo "Dezembro"."\n";
+     break;
+ }
+return $mes;
+}
+
+
+print "Digite qualquer nÃºmero entre 1 a 12, e veja qual mÃªs ele corresponde.\n\n" ;
+$mes= (int) fgets (STDIN);
+
+MesNum ($mes);
