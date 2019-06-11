38f2a4170454d0bb711543ec48bfb5f9de697e92
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-06-10 22:05:21 -0300

    Update exer2.php

--- exer2.php
+++ exer2.php
@@ -1,1 +1,18 @@
 <?php
+
+function calcular ($Ca, $Cb){
+    $hip = sqrt (($Ca * $Ca) + ($Cb * $Cb));
+    return ($hip);
+}
+
+print "Digite o valor do cateto A: ";
+$Ca = (float) fgets (STDIN);
+
+
+print "\n\nDigite o valor do cateto B: ";
+$Cb = (float) fgets (STDIN);
+
+$hip = calcular ($Ca, $Cb);
+print "\n\nO valor da hiipotenusa é : $hip";
+
+print "Fim Algoritmo" ;
